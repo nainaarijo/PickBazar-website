e62558d790ffc553daefdac8ef946b0cef051265
nainaarijo <nainaarijo123@gmail.com>
2024-12-14 13:44:34 +0500

    navbar and hero section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderSection from "./components/HeaderSection";
 import HeroSection from "./components/HeroSection"; 
+import slideCards from "./components/slideCards";
 
 function App() {
   return (
     <>
       <HeaderSection />
       <HeroSection />
+      <slideCards />
     </>
   );
 }
--- src/components/slideCards.jsx
+++ src/components/slideCards.jsx
@@ -41,5 +41,5 @@ const Image = () => {
   );
 };
 
-export default swiperImage;
+export default slideCards;
 
